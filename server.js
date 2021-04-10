d13b3bcf04b442f2de158901a6ecac8ea125ee85
Ramyashree HC <ramyashreehc21@gmail.com>
2021-04-10 20:57:24 +0530

    hotel

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const cors = require('cors');
 const db = require('./db');
 const port = 5000;
 const path = require('path')
-const __variableOfChoice = path.resolve()
+// const __variableOfChoice = path.resolve()
 
 app.use(cors());
 app.use(express.json())
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__variableOfChoice,"/frontend/build")))
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__variableOfChoice,"/frontend/build")))
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__variableOfChoice,"frontend","build","index.html"))
-    })
-}else{
-    app.get("/",(req,res)=>{
-        res.send("api is running")
-    })
-}
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__variableOfChoice,"frontend","build","index.html"))
+//     })
+// }else{
+//     app.get("/",(req,res)=>{
+//         res.send("api is running")
+//     })
+// }
 
 
 app.listen(port, () => {
